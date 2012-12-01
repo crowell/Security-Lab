9a62f019adba2370cdefcf7af1a1d4689c353298
Ben Duong <bduong@bu.edu>
2012-12-01 15:16:02 -0500

    Added print stuff code

---
+++ printStuff.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+printf("k");
+printf("e");
+printf("y");
+printf("{");
+printf("n");
+printf("o");
+printf("_");
+printf("s");
+printf("t");
+printf("r");
+printf("i");
+printf("n");
+printf("g");
+printf("s");
+printf("}\n");
+
+return 0;
+}
