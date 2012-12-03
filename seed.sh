04671a10c9b9ecf733ce7ca1e9941cb718bd2454
Ben Duong <bduong@bu.edu>
2012-12-03 11:37:55 -0500

    Added 20 more tables

--- seed.sh
+++ seed.sh
@@ -8,4 +8,35 @@ $MYSQLD -e "CREATE TABLE users (ID MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY
 $MYSQLD -e "INSERT INTO users (username, password) VALUES ('admin', '6ea27872aeabcec0a4d74697e1786225');"
 $MYSQLD -e "INSERT INTO users (username, password) VALUES ('James Bond', 'b5117cab4abf1b39ff9a03a3a3f2862c');"
 $MYSQLD -e "CREATE TABLE secrets (ID MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY, secret VARCHAR(200));"
-$MYSQLD -e "CREATE TABLE challenges (ID MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY, score INT, challenge VARCHAR(60));"
+
+TABLES=(
+"products"
+"admins"
+"offices"
+"executives"
+"secrets"
+"challenges"
+"labs"
+"hardware"
+"software"
+"devices"
+"vendors"
+"partners"
+"affiliates"
+"customers"
+"answers"
+"questions"
+"meaningOfLife"
+"pokemon"
+"patents"
+"lawsuits"
+"copyrights"
+"tradeSecrets"
+);
+
+for name in ${TABLES[@]}; do
+$MYSQLD -e "CREATE TABLE ${name} (ID MEDIUMINT NOT NULL AUTO_INCREMENT PRIMARY KEY, company_id INT, string VARCHAR(30));"
+done
+
+
+
