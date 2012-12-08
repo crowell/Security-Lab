c5a7c461aeaec34bc47de032db1039c830c4ac96
Ben Duong <bduong@vmware.com>
2012-12-08 14:24:40 -0500

    changed setup.sh to start services

--- setup.sh
+++ setup.sh
@@ -1,5 +1,26 @@
+echo_bold () {
+        message=${1} 
+        echo -e "\033[1m${message}\033[0m"
+}
+echo_bold "Copying Apache2 config..."
 cp apache2.conf /etc/apache2/apache2.conf
+echo_bold "Clearing web server files...."
 rm -fr /var/www/*
+echo_bold "Copying files to web server...."
 cp -R lab/* /var/www/
+echo_bold "Changing permissions....."
 ./chmodder.sh
+
+echo_bold "Starting mysql........."
+mysql_status=$(service mysql start |grep "already running");
+if [ -z "$mysql_status"]
+then
+        service mysql restart
+fi
+
+echo_bold "Seeding the mysql database........."
+./seed.sh
+
+echo_bold "Starting apache2..........."
+service apache2 restart
 
